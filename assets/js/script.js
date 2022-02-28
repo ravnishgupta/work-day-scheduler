563571ae7bce9186030253f5ca032bc852b0a37d
Ravnish Gupta <ravnish@gmail.com>
2022-02-27 18:23:31 -0800

    removed hard-coded rows, now generating them dynamically and also showing the correct color based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,49 @@
-//$( document ).ready(function() {
-    $("#currentDay").text(moment().format('dddd'));
-//});
+$( document ).ready(function() {
+    $("#currentDay").text(moment().format('dddd, MMMM Do'));
+});
+
+function createDiv(){
+    var mainDiv = $(".container");
+   
+
+    //populate the main div container
+    for (var i=0; i<9; i++) {
+        var newDiv = document.createElement("div");
+        newDiv.className = "row";
+        newDiv.id = i+9;
+        mainDiv.append(newDiv);
+    }
+
+    //populate inner Divs
+     for (var i=9; i<18; i++) {
+
+        currHr = moment().format('HH')
+         var innerDiv = $("#" + i);
+         var timeDiv = document.createElement("div");
+         timeDiv.className = "col-1 hour";
+         timeDiv.id = i;
+         timeDiv.textContent = moment(i, ["HH.mm"]).format("h A");
+    
+         var taskDiv = document.createElement("div");
+         taskDiv.id = i;
+        if (currHr>i) {
+            taskDiv.className = "col-10 past";
+        }
+        else if (currHr === i) {
+            taskDiv.className = "col-10 present";
+        }
+        else taskDiv.className = "col-10 future";
+
+         var saveDiv = document.createElement("div");
+         saveDiv.className = "col-1 saveBtn"
+         saveDiv.id = i;
+
+        innerDiv.append(timeDiv);
+        innerDiv.append(taskDiv);
+        innerDiv.append(saveDiv);
+
+    }
+
+}
+
+createDiv()
