5a0d2c746b1a205fb43b9fef7b209ad8eeab01e6
Ravnish Gupta <ravnish@gmail.com>
2022-02-27 23:31:04 -0800

    enable adding/editing tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-$( document ).ready(function() {
+//$( document ).ready(function() {
     $("#currentDay").text(moment().format('dddd, MMMM Do'));
-});
+//});
 
 function createDiv(){
     var mainDiv = $(".container");
@@ -17,6 +17,10 @@ function createDiv(){
      for (var i=9; i<18; i++) {
 
         currHr = moment().format('HH')
+        var spanEl = document.createElement("span");
+        var pEl = document.createElement("p");
+
+        //spanEl.append(pEl);
         var innerDiv = $("#" + i);
         var timeDiv = document.createElement("div");
         timeDiv.className = "col-1 hour";
@@ -24,26 +28,63 @@ function createDiv(){
         timeDiv.textContent = moment(i, ["HH.mm"]).format("h A");
     
         var taskDiv = document.createElement("div");
-        taskDiv.id = i;
+        taskDiv.setAttribute("data-time-id", i.toString());
+        //taskDiv.innerHTML = "<p>";
+
         if (currHr>i) {
             taskDiv.className = "col-10 past";
+            spanEl.className = "col-10 past";
         }
         else if (currHr === i) {
             taskDiv.className = "col-10 present";
+            spanEl.className = "col-10 present";
         }
         else { 
             taskDiv.className = "col-10 future";
+            spanEl.className = "col-10 future";
         }
-        
+        taskDiv.className = taskDiv.className + " task"
+
+       pEl.className = taskDiv.className;
+        taskDiv.append(pEl);
+
         var saveDiv = document.createElement("div");
         saveDiv.className = "col-1 saveBtn fa fa-save"
         saveDiv.id = i;
         innerDiv.append(timeDiv);
-        innerDiv.append(taskDiv);
+        innerDiv.append(spanEl);
         innerDiv.append(saveDiv);
 
     }
 
 }
 
-createDiv()
+
+$(".container").on("click", "span", function() {
+
+    var text = $(this)
+    .text()
+    .trim();
+    var textInput = $("<textarea>").addClass($(this).attr("class")).val(text);
+    
+    $(this).replaceWith(textInput)
+    textInput.trigger("focus");
+   
+});
+
+// editable field was un-focused
+$(".container").on("blur", "textarea", function() {
+    // get current value of textarea
+    var text = $(this).val();
+
+    // recreate <span> element
+    var taskSpan = $("<span>")
+    .addClass($(this).attr("class"))
+    .text(text);
+  
+    // replace textarea with new content
+    $(this).replaceWith(taskSpan);
+
+});
+
+createDiv();
