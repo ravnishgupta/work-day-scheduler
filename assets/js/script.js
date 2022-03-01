de8107cca4ded69ff931459b138c5266ba9bcf37
Ravnish Gupta <ravnish@gmail.com>
2022-02-28 19:51:19 -0800

    for debugging

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,46 +12,39 @@ function createDiv(){
         mainDiv.append(newDiv);
     }
 
-    //populate inner Divs
+    //populate inner divs
      for (var i=9; i<18; i++) {
 
         currHr = moment().format('HH')
         var spanEl = document.createElement("span");
-       // var pEl = document.createElement("p");
-
-        //spanEl.append(pEl);
         var innerDiv = $("#" + i);
         var timeDiv = document.createElement("div");
         timeDiv.className = "col-1 hour";
-        timeDiv.id = i;
+        timeDiv.id += i
         timeDiv.textContent = moment(i, ["HH.mm"]).format("h A");
-    
-        // var taskDiv = document.createElement("div");
-        // taskDiv.setAttribute("data-time-id", i.toString());
-        spanEl.setAttribute("data-time-id", i.toString())
-        //taskDiv.innerHTML = "<p>";
+
+        spanEl.setAttribute("data-span-time-id", (i-9).toString());
 
         if (currHr>i) {
-          //  taskDiv.className = "col-10 past";
             spanEl.className = "col-10 past";
         }
         else if (currHr === i) {
-           // taskDiv.className = "col-10 present";
             spanEl.className = "col-10 present";
         }
         else { 
-          //  taskDiv.className = "col-10 future";
             spanEl.className = "col-10 future";
         }
-       // taskDiv.className = taskDiv.className + " task"
-        spanEl.className = spanEl.className + " task"
-
-      // pEl.className = taskDiv.className;
-      //  taskDiv.append(pEl);
+        spanEl.className = spanEl.className + " task";
 
         var saveDiv = document.createElement("div");
-        saveDiv.className = "col-1 saveBtn fa fa-save"
+        var iEl  = document.createElement("i");
+
+        iEl.className = "fa fa-save my-4 mx-1"
+        iEl.setAttribute("data-time-id", (i-9).toString());
+
+        saveDiv.className = "col-1 saveBtn"
         saveDiv.id = i;
+        saveDiv.append(iEl)
         innerDiv.append(timeDiv);
         innerDiv.append(spanEl);
         innerDiv.append(saveDiv);
@@ -61,13 +54,23 @@ function createDiv(){
 }
 
 
+$(".container").on("click", "i", function(){
+    //var timeID = $(this).attr("data-time-id");
+    debugger;
+    var obj = $(this).parents("span");
+});
+
 $(".container").on("click", "span", function() {
 
     var text = $(this)
     .text()
     .trim();
+
+    var timeID = $(this).attr('data-span-time-id');
+
     var textInput = $("<textarea>").addClass($(this).attr("class")).val(text);
-    
+    textInput.attr("data-span-time-id", timeID)
+
     $(this).replaceWith(textInput)
     textInput.trigger("focus");
    
@@ -77,28 +80,37 @@ $(".container").on("click", "span", function() {
 $(".container").on("blur", "textarea", function() {
     // get current value of textarea
     var text = $(this).val();
+    var timeID = $(this).attr('data-span-time-id');
 
     // recreate <span> element
     var taskSpan = $("<span>")
     .addClass($(this).attr("class"))
     .text(text);
+   
+    taskSpan.attr("data-span-time-id", timeID)
   
     // replace textarea with new content
     $(this).replaceWith(taskSpan);
 
 });
 
-function saveTasks() {
-    var taskSpan = $(".task");
+function saveTasks(timeID) {
+   // var taskSpan = $("span");
+   console.log($('.container').parents().attr("data-span-time-id", '0'))
+    //$("span").find("[data-time-id='" + timeID + "']" );
+    //console.log(taskSpan);
+
+
+   //console.log( $("span").find("[data-time-id='" + timeID + "']" ));
     
-    for (var i=0; i<taskSpan.length; i++) {
-        var thisTask = {};
-        thisTask.time = taskSpan[i].getAttribute("data-time-id");
-        thisTask.description = taskSpan[i].textContent;
-        dailyTasks.push(thisTask);
-    }
+    // for (var i=0; i<taskSpan.length; i++) {
+    //     var thisTask = {};
+    //     thisTask.time = taskSpan[i].getAttribute("data-time-id");
+    //     thisTask.description = taskSpan[i].textContent;
+    //     dailyTasks.push(thisTask);
+    // }
 
 }
 
 createDiv();
-saveTasks();
+//saveTasks();
